bd19e8d8684a77f9a92dedeba0bfc23b17f6b30f
GunaDhivyaVarshini-V <vgdv242004@gmail.com>
2025-07-06 21:12:49 +0530

    update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,11 @@
 const express=require("express");
 const app=express();
 app.use(express.json());
-app.use("/",(req,res)=>{
-    res.data({
-        sucess:true,
-        message:"hello world"})
+app.use("/",(req,res,next)=>{
+    next();
+    // res.data({
+    //     sucess:true,
+    //     message:"hello world"})
 })
 app.post('/submit',(req,res)=>{
     console.log(req.body);
